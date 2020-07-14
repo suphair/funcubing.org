2201543d617b32e4386ee2dc65a0a424428fb5b2
SUPHAIR <suphair@gmail.com>
2020-07-14 08:40:53 +0300

    cron

--- Pages/cron/index.php
+++ Pages/cron/index.php
@@ -1,12 +1,13 @@
 <?php
+
 if ((wcaoauth::me()->wca_id ?? FALSE) == config::get('Admin', 'wcaid')
         or filter_input(INPUT_SERVER, 'HTTP_USER_AGENT') == 'Wget/1.17.1 (linux-gnu)'
         or config::isLocalhost()) {
     $cron = new cron(db::connection());
     $cron->run();
     db::close();
-    die('Cron is complete');
+    die(json_encode(['message' => 'Cron is complete']));
 } else {
-    die('Access denied');
+    die(json_encode(['error' => 'Access denied']));
 }
 
