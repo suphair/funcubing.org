e54bc359a69e2b2c53ca770836ec82a3791f9fc2
SUPHAIR <suphair@gmail.com>
2020-07-16 16:32:34 +0300

    Update functions.php

--- Pages/friends/functions.php
+++ Pages/friends/functions.php
@@ -5,11 +5,7 @@ namespace friends;
 function cron() {
     $friends = \db::rows("SELECT DISTINCT friend FROM friends");
     foreach ($friends as $friend) {
-        $user = \wcaapi::getUserCompetitionsUpcoming(
-                        $friend->friend
-                        , __FILE__ . ': ' . __LINE__,FALSE);
-        print_r($user->user );
-        echo '<hr>';
+        $user = \wcaapi::getUserCompetitionsUpcoming($friend->friend, __FILE__ . ': ' . __LINE__, FALSE);
         \competitor\actual($user->user ?? FALSE);
     }
     return count($friends);
