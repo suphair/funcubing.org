9c82a41802b5b4c3692832683ca93005d091ba3b
SUPHAIR <suphair@gmail.com>
2020-07-16 18:59:15 +0300

    error #14

--- Pages/unofficial/post.competition.delete.php
+++ Pages/unofficial/post.competition.delete.php
@@ -1,5 +1,6 @@
 <?php
 
+db::exec("DELETE FROM `unofficial_organizers` WHERE competition = $comp->id");
 db::exec("DELETE FROM `unofficial_competitions` WHERE ID = $comp->id");
 
 if (db::affected()) {
