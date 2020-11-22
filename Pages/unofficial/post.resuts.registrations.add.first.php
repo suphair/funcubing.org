e0f6d9649ffe53b92782c4cf2e9ab9c7d6f78d5c
SUPHAIR <suphair@gmail.com>
2020-11-22 13:08:14 +0300

    Update post.resuts.registrations.add.first.php

--- Pages/unofficial/post.resuts.registrations.add.first.php
+++ Pages/unofficial/post.resuts.registrations.add.first.php
@@ -4,6 +4,7 @@ $competitors = filter_input(INPUT_POST, 'competitors', FILTER_DEFAULT, FILTER_RE
 
 $code = db::escape(request(3));
 $round = db::escape(request(4));
+if(is_array($competitors)){
 
 if ($code and is_numeric($round)) {
     foreach (array_keys($competitors) as $competitor) {
@@ -16,4 +17,5 @@ if ($code and is_numeric($round)) {
                 . " AND unofficial_events_rounds.round = $round "
                 . " AND unofficial_events_dict.code = '$code' ");
     }
+}
 }
